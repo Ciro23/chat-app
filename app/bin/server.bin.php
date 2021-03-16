574c43910f7e76ec3db679e1311497de5e2638ca
Ciro Sorre <ciro.sorre23@gmail.com>
2021-03-16 11:18:47 +0100

    the port is now given by the .env file

--- app/bin/server.bin.php
+++ app/bin/server.bin.php
@@ -6,13 +6,17 @@ use GSoares\RatchetChat\Chat;
 
 require __DIR__ . '/../../vendor/autoload.php';
 
+// saves the env vars in the $_ENV superglobal
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../config/");
+$dotenv->load();
+
 $server = IoServer::factory(
     new HttpServer(
         new WsServer(
             new \Chat()
         )
     ),
-    8080
+    $_ENV['port']
 );
 
 $server->run();
