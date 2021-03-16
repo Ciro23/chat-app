9979553c679fbc77af19e3aff49e6291d0893933
Ciro Sorre <ciro.sorre23@gmail.com>
2021-03-16 18:56:33 +0100

    added the link to the style and changed the value of the submit button

--- app/views/setusername.view.php
+++ app/views/setusername.view.php
@@ -5,11 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Set username - Chat App</title>
+
+    <link rel="stylesheet" href="/assets/styles/css/set-username.style.css">
 </head>
 <body>
     <form action="/set-username/action" method="POST">
         <input type="text" placeholder="Your name" name="username" maxlength="20">
-        <input type="submit" value="Set">
+        <input type="submit" value="Chat">
     </form>
 </body>
 </html>
